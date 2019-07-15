5dda11349806c0e570e776a06e0bb210c2221a00
Arvin Agas <arvagas@gmail.com>
2019-07-15 18:09:37 -0400

    added quarter button with working state

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import TopRow from "./TopRow"
 import BottomRow from "./BottomRow";
 import HomeButtons from "./HomeButtons"
 import AwayButtons from "./AwayButtons"
+import QuarterButtons from "./QuarterButtons"
 
 function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
   const [homeScore, setHomeScore] = useState(0)
   const [awayScore, setAwayScore] = useState(0)
+  const [quarter, setQuarter] = useState(1)
 
   return (
     <div className="container">
       <section className="scoreboard">
-        <TopRow homeScore={homeScore} awayScore={awayScore}/>
-        <BottomRow />
+        <TopRow homeScore={homeScore} awayScore={awayScore} />
+        <BottomRow quarter={quarter} />
       </section>
       <section className="buttons">
         <HomeButtons homeScore={homeScore} setHomeScore={setHomeScore} />
         <AwayButtons awayScore={awayScore} setAwayScore={setAwayScore} />
+        <QuarterButtons quarter={quarter} setQuarter={setQuarter} />
       </section>
     </div>
-  );
+  )
 }
 
 export default App;
